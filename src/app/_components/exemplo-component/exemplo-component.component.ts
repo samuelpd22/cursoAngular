0efabcbef614c54cc64f050c6610bb2ce9e38610
samuelpd22 <samuel050101@hotmail.com>
2024-04-09 19:48:12 -0300

    inserindo botão em um component

---
+++ src/app/_components/exemplo-component/exemplo-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-component',
+  templateUrl: './exemplo-component.component.html',
+  styleUrl: './exemplo-component.component.css'
+})
+export class ExemploComponentComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ExemploComponentComponent } from './_components/exemplo-component/exemplo-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ExemploComponentComponent
   ],
   imports: [
     BrowserModule,
